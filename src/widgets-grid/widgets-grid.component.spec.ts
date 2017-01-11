860cb446297e49352966fa427a9e4e6484c16f8a
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-01-11 16:41:25 +0200

    Widgets grid tests

--- src/widgets-grid/widgets-grid.component.spec.ts
+++ src/widgets-grid/widgets-grid.component.spec.ts
@@ -9,33 +9,38 @@ import { WidgetsGridModule } from './widgets-grid.module';
 import { ComponentDetails } from "./component-details.model";
 
 describe('WidgetsGridComponent', () => {
+	let componentDetailsGenerator = (id: any, html: string) => {
+		return <ComponentDetails>{
+			gridItemConfig: {
+				sizex: 2,
+				sizey: 1,
+				fixed: true
+			},
+			id: id,
+			name: 'Demo1',
+			html: html
+		}
+	};
+
 	describe('WidgetsGridComponent components insertion', () => {
 		beforeEach(() => {
 			TestBed.configureTestingModule({
 				imports: [WidgetsGridModule],
-				declarations: [TestComponent],
+				declarations: [SimpleComponent],
 			});
 		});
 
 		it('Should handle 1 simple component', async(() => {
 			TestBed.compileComponents().then(() => {
 				// Arrange
-				let fixture = TestBed.createComponent(TestComponent);
+				let fixture = TestBed.createComponent(SimpleComponent);
 				let widgetsGridComponent: WidgetsGridComponent = fixture.debugElement.children[0].componentInstance;
 				let widgetsGridDOMElement = fixture.debugElement.children[0].nativeElement;
 
-				WidgetsManagerService.provideWidgetsModule(TestModule);
+				WidgetsManagerService.provideWidgetsModule(MockModule);
 
-				widgetsGridComponent.componentsDetails = [<ComponentDetails>{
-					gridItemConfig: {
-						sizex: 2,
-						sizey: 1,
-						fixed: true
-					},
-					id: 1,
-					name: 'Demo1',
-					html: '<div>Test</div>'
-				}];
+				widgetsGridComponent.componentsDetails = [];
+				widgetsGridComponent.componentsDetails.push(componentDetailsGenerator(1, '<div>Test</div>'));
 
 				// Act
 				fixture.detectChanges();
@@ -50,29 +55,43 @@ describe('WidgetsGridComponent', () => {
 		it('Should handle more than 1 simple components', async(() => {
 			TestBed.compileComponents().then(() => {
 				// Arrange
-				let fixture = TestBed.createComponent(TestComponent);
+				let fixture = TestBed.createComponent(SimpleComponent);
 				let widgetsGridComponent: WidgetsGridComponent = fixture.debugElement.children[0].componentInstance;
 				let widgetsGridDOMElement = fixture.debugElement.children[0].nativeElement;
 
-				// let someModule: TypeMoq.IMock<NgModule> = TypeMoq.Mock.ofType(NgModule);
-				WidgetsManagerService.provideWidgetsModule(TestModule);
+				WidgetsManagerService.provideWidgetsModule(MockModule);
 
-				widgetsGridComponent.componentsDetails = [<ComponentDetails>{
-					gridItemConfig: {
-						sizex: 2,
-						sizey: 1,
-						fixed: true
-					},
-					id: 1,
-					name: 'Demo1',
-					html: '<div>Test</div>'
-				}];
+				widgetsGridComponent.componentsDetails.push(componentDetailsGenerator(1, '<div>Test1</div>'));
+				widgetsGridComponent.componentsDetails.push(componentDetailsGenerator(2, '<div>Test2</div>'));
 
 				// Act
 				fixture.detectChanges();
 
 				// Assert
-				expect(widgetsGridDOMElement.querySelectorAll('component-factory').length).toEqual(1);
+				expect(widgetsGridDOMElement.querySelectorAll('component-factory').length).toEqual(2);
+
+				WidgetsManagerService.widgetsModule = undefined;
+			});
+		}));
+
+		it('Should contain the same HTML passed to the component', async(() => {
+			TestBed.compileComponents().then(() => {
+				// Arrange
+				let fixture = TestBed.createComponent(SimpleComponent);
+				let widgetsGridComponent: WidgetsGridComponent = fixture.debugElement.children[0].componentInstance;
+				let widgetsGridDOMElement = fixture.debugElement.children[0].nativeElement;
+
+				WidgetsManagerService.provideWidgetsModule(MockModule);
+
+				widgetsGridComponent.componentsDetails = [];
+				widgetsGridComponent.componentsDetails.push(componentDetailsGenerator(1, '<div>Test1</div>'));
+
+				// Act
+				fixture.detectChanges();
+
+				// Assert
+				let componentFactory = widgetsGridDOMElement.querySelectorAll('component-factory')[0];
+				expect(componentFactory.getAttribute('ng-reflect-html')).toEqual(widgetsGridComponent.componentsDetails[0].html);
 
 				WidgetsManagerService.widgetsModule = undefined;
 			});
@@ -84,11 +103,11 @@ describe('WidgetsGridComponent', () => {
 	selector: 'test-cmp',
 	template: '<widgets-grid></widgets-grid>'
 })
-class TestComponent {
+class SimpleComponent {
 }
 
 @NgModule({
 	imports: [CommonModule],
 })
-export class TestModule {
+class MockModule {
 }
