815f7d19bb9ed5b42d99a13ecade106a51f418e2
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-02-05 15:20:10 +0200

    angular2-webpack-library-seed main.ts build-up

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-export * from './component-factory/component-factory.module';
-export * from './widgets-grid/widgets-grid.module';
-export * from './widgets-manager.module';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { WidgetsGridModule } from './widgets-grid/widgets-grid.module';
+import { WidgetsManagerService } from './widgets-manager.service';
+
+@NgModule({
+	imports: [WidgetsGridModule],
+	providers: [WidgetsManagerService],
+	exports: [WidgetsGridModule]
+})
+export class WidgetsManagerModule {
+	static forRoot(): ModuleWithProviders {
+		return {
+			ngModule: WidgetsManagerModule
+		};
+	}
+}
+
+export * from 'angular2-grid';
+export * from './widgets-grid/index';
+export * from './component-factory/index';
+export * from './widgets-manager.service';
