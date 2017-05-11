2fc3f829d040a0164433fe696d657cb10272fc58
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-05-11 09:16:09 +0300

    concat array typing fix

--- src/component-factory/component-factory.component.ts
+++ src/component-factory/component-factory.component.ts
@@ -25,7 +25,7 @@ export function createComponentFactory(compiler: Compiler, metadata: Component):
 	}
 
 	@NgModule({
-		imports: [CommonModule, RouterModule].concat(WidgetsManagerService.widgetsModule),
+		imports: [CommonModule, RouterModule].concat(<[any]>WidgetsManagerService.widgetsModule),
 		declarations: [decoratedComponent]
 	})
 	class DynamicHtmlModule {
